c306df3154a3b329f5ee45c212f9e42163788f02
Viktoriia Fenenko <v.l.fenenko@gmail.com>
2021-06-13 11:30:38 +0300

    Выполение задания

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import shortid from 'shortid';
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+	return (
+		<>
+			{options.map((option) => (
+				<button className={styles.button} key={shortid.generate()} type="button" name={option} onClick={onLeaveFeedback}>
+					{option}
+				</button>
+			))}
+		</>
+	);
+};
+FeedbackOptions.propTypes = {
+	options: PropTypes.array.isRequired,
+	onLeaveFeedback: PropTypes.func.isRequired
+};
+
+export default FeedbackOptions;
